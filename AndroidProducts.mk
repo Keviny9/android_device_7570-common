5d0a5f53a1d0a922bb05b194c5fde07c593ffaef
Keviny9 <kevinyoliveira9@gmail.com>
2021-12-31 13:19:15 -0300

    Device Tree for CRDroid 6.1

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_on5xelte.mk 
+
+    
+COMMON_LUNCH_CHOICES := \
+    lineage_on5xelte-user \
+    lineage_on5xelte-userdebug \
+    lineage_on5xelte-eng
---
+++ lineage_on5xelte.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#               2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from on5xelte device
+$(call inherit-product, device/samsung/on5xelte/universal7570-common.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_on5xelte
+PRODUCT_DEVICE := on5xelte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-G570M
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="on5xelte-userdebug 10 QQ3A 200905 001 210925 test-keys " \
+    PRODUCT_NAME=on5xelte \
+    TARGET_DEVICE := SM-G570M
+
+
+BUILD_FINGERPRINT := samsung/on5xelte/SM-G570M:10/QQ3A/200905.001/210925:userdebug/test-keys 
+
+TARGET_VENDOR := on5xelte
