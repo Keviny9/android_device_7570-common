6f420196b05e21522548c0833b0ae2c9a12363af
Keviny9 <kevinyoliveira9@gmail.com>
2022-04-03 21:47:53 -0300

    Device Tree for aosp_12l

---
+++ Android.mk
@@ -0,0 +1,17 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+LOCAL_PATH := $(call my-dir)
+
+#ifneq ($(filter on5xelte, $(TARGET_DEVICE)),)
+
+#include $(call all-makefiles-under,$(LOCAL_PATH))
+
+#endif
+
+ifeq ($(TARGET_DEVICE),on5xelte)
+ 
+ include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ include $(CLEAR_VARS)
+ 
+endif
